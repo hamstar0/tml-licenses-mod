c98075ce4fd7a6798336fef04176ef30c0c0ad9d
hamstar0 <webutek@gmail.com>
2018-06-14 04:04:27 -0700

    v2.0.0 * Restructured player load

--- LicensesPlayer.cs
+++ LicensesPlayer.cs
@@ -1,6 +1,5 @@
 ﻿using HamstarHelpers.DebugHelpers;
 using HamstarHelpers.ItemHelpers;
-using HamstarHelpers.TmlHelpers;
 using Licenses.Items;
 using Microsoft.Xna.Framework;
 using Nihilism;
@@ -8,11 +7,10 @@ using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.ModLoader.IO;
 
 
 namespace Licenses {
-	class LicensesPlayer : ModPlayer {
+	partial class LicensesPlayer : ModPlayer {
 		private readonly ISet<string> PendingLoadLicenses = new HashSet<string>();
 		public ISet<string> LicensedItems { get; private set; }
 
@@ -25,37 +23,6 @@ namespace Licenses {
 
 		public override void Initialize() {
 			this.LicensedItems = new HashSet<string>();
-		}
-
-		public override void Load( TagCompound tag ) {
-			var self = this;
-			var mymod = (LicensesMod)this.mod;
-
-			this.LicensedItems.Clear();
-
-			if( tag.ContainsKey("license_count") ) {
-				int count = tag.GetInt( "license_count" );
-				string[] licenses = new string[count];
-
-				for( int i=0; i<count; i++ ) {
-					string item_name = tag.GetString( "license_" + i );
-
-					this.PendingLoadLicenses.Add( item_name );
-				}
-			}
-		}
-
-		public override TagCompound Save() {
-			var tags = new TagCompound {
-				{ "license_count", this.LicensedItems.Count }
-			};
-
-			int i = 0;
-			foreach( string name in this.LicensedItems ) {
-				tags["license_" +i++] = name;
-			}
-
-			return tags;
 		}
 
 
@@ -77,60 +44,6 @@ namespace Licenses {
 			} else if( Main.netMode == 1 ) {
 				this.OnEnterWorldForClient();
 			}
-		}
-		
-		////////////////
-
-		private void OnEnterWorldLocal() {
-			var mymod = (LicensesMod)this.mod;
-
-			foreach( string item_name in this.PendingLoadLicenses ) {
-				this.SetItemNameLicense( item_name, false );
-			}
-			this.PendingLoadLicenses.Clear();
-
-			TmlLoadHelpers.TriggerCustomPromise( "LicensesOnEnterWorld" );
-			TmlLoadHelpers.AddWorldUnloadOncePromise( () => {
-				TmlLoadHelpers.ClearCustomPromise( "LicensesOnEnterWorld" );
-			} );
-		}
-
-		public void OnEnterWorldForSingle() {
-			TmlLoadHelpers.AddCustomPromise( "LicensesOnGameModeLoad", () => {
-				this.OnEnterWorldLocal();
-				return false;
-			} );
-		}
-
-		public void OnEnterWorldForClient() {
-			TmlLoadHelpers.AddCustomPromise( "LicensesOnGameModeLoad", () => {
-				this.OnEnterWorldLocal();
-				return false;
-			} );
-		}
-
-		public void OnEnterWorldForServer() {
-			TmlLoadHelpers.AddCustomPromise( "LicensesOnGameModeLoad", () => {
-				TmlLoadHelpers.TriggerCustomPromise( "LicensesOnEnterWorld" );
-				TmlLoadHelpers.AddWorldUnloadOncePromise( () => {
-					TmlLoadHelpers.ClearCustomPromise( "LicensesOnEnterWorld" );
-				} );
-				return false;
-			} );
-		}
-
-
-		////////////////
-
-		public override void SetupStartInventory( IList<Item> items ) {
-			var mymod = (LicensesMod)this.mod;
-			if( mymod.Config.NewPlayerStarterLicenses == 0 ) { return; }
-
-			Item licenses = new Item();
-			licenses.SetDefaults( mymod.ItemType<LicenseItem>(), true );
-			licenses.stack = mymod.Config.NewPlayerStarterLicenses;
-
-			items.Add( licenses );
 		}
 
 
---
+++ LicensesPlayer_Load.cs
@@ -0,0 +1,98 @@
+﻿using HamstarHelpers.DebugHelpers;
+using HamstarHelpers.TmlHelpers;
+using Licenses.Items;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+
+namespace Licenses {
+	partial class LicensesPlayer : ModPlayer {
+		public override void Load( TagCompound tag ) {
+			var self = this;
+			var mymod = (LicensesMod)this.mod;
+
+			this.LicensedItems.Clear();
+
+			if( tag.ContainsKey("license_count") ) {
+				int count = tag.GetInt( "license_count" );
+				string[] licenses = new string[count];
+
+				for( int i=0; i<count; i++ ) {
+					string item_name = tag.GetString( "license_" + i );
+
+					this.PendingLoadLicenses.Add( item_name );
+				}
+			}
+		}
+
+		public override TagCompound Save() {
+			var tags = new TagCompound {
+				{ "license_count", this.LicensedItems.Count }
+			};
+
+			int i = 0;
+			foreach( string name in this.LicensedItems ) {
+				tags["license_" +i++] = name;
+			}
+
+			return tags;
+		}
+
+		
+		////////////////
+
+		private void OnEnterWorldLocal() {
+			var mymod = (LicensesMod)this.mod;
+
+			foreach( string item_name in this.PendingLoadLicenses ) {
+				this.SetItemNameLicense( item_name, false );
+			}
+			this.PendingLoadLicenses.Clear();
+
+			TmlLoadHelpers.TriggerCustomPromise( "LicensesOnEnterWorld" );
+			TmlLoadHelpers.AddWorldUnloadOncePromise( () => {
+				TmlLoadHelpers.ClearCustomPromise( "LicensesOnEnterWorld" );
+			} );
+		}
+
+		public void OnEnterWorldForSingle() {
+			TmlLoadHelpers.AddCustomPromise( "LicensesOnGameModeLoad", () => {
+				this.OnEnterWorldLocal();
+				return false;
+			} );
+		}
+
+		public void OnEnterWorldForClient() {
+			TmlLoadHelpers.AddCustomPromise( "LicensesOnGameModeLoad", () => {
+				this.OnEnterWorldLocal();
+				return false;
+			} );
+		}
+
+		public void OnEnterWorldForServer() {
+			TmlLoadHelpers.AddCustomPromise( "LicensesOnGameModeLoad", () => {
+				TmlLoadHelpers.TriggerCustomPromise( "LicensesOnEnterWorld" );
+				TmlLoadHelpers.AddWorldUnloadOncePromise( () => {
+					TmlLoadHelpers.ClearCustomPromise( "LicensesOnEnterWorld" );
+				} );
+				return false;
+			} );
+		}
+
+
+		////////////////
+
+		public override void SetupStartInventory( IList<Item> items ) {
+			var mymod = (LicensesMod)this.mod;
+			if( mymod.Config.NewPlayerStarterLicenses == 0 ) { return; }
+
+			Item licenses = new Item();
+			licenses.SetDefaults( mymod.ItemType<LicenseItem>(), true );
+			licenses.stack = mymod.Config.NewPlayerStarterLicenses;
+
+			items.Add( licenses );
+		}
+	}
+}

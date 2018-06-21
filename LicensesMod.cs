835d5a823776c892863c225d895ff09d03faf52a
hamstar0 <webutek@gmail.com>
2018-06-20 17:28:32 -0700

    v2.0.1 * Update to C# 6 * Fixed Nihilism incorrectly loading blacklists * Internal restructuring

--- LicensesMod.cs
+++ LicensesMod.cs
@@ -1,11 +1,6 @@
 ﻿using HamstarHelpers.Components.Config;
-using HamstarHelpers.DebugHelpers;
 using HamstarHelpers.Services.EntityGroups;
-using HamstarHelpers.Services.Messages;
 using HamstarHelpers.TmlHelpers;
-using Nihilism;
-using Rewards;
-using Rewards.Items;
 using System;
 using System.IO;
 using Terraria;
@@ -13,7 +8,7 @@ using Terraria.ModLoader;
 
 
 namespace Licenses {
-    public class LicensesMod : Mod {
+    public partial class LicensesMod : Mod {
 		public static LicensesMod Instance { get; private set; }
 
 		public static string GithubUserName { get { return "hamstar0"; } }
@@ -95,90 +90,6 @@ namespace Licenses {
 			Array.Copy( args, 1, new_args, 0, args.Length - 1 );
 
 			return LicensesAPI.Call( call_type, new_args );
-		}
-
-
-		////////////////
-
-		public void LoadGameMode() {
-			NihilismAPI.SuppressAutoSavingOn();
-			RewardsAPI.SuppressConfigAutoSavingOn();
-			
-			NihilismAPI.SetItemBlacklistEntry( "All Items", true );
-
-			if( this.Config.OverrideNihilismDefaultFilters ) {
-				if( this.Config.FreeRecipes ) {
-					NihilismAPI.ClearRecipeBlacklist( true );
-				} else {
-					NihilismAPI.SetRecipeBlacklistEntry( "Any Item", true );
-				}
-				NihilismAPI.ClearItemBlacklist( true );
-				NihilismAPI.ClearNpcBlacklist( true );
-				NihilismAPI.ClearNpcLootBlacklist( true );
-			}
-
-			foreach( string name in this.Config.FreeStarterItems ) {
-				NihilismAPI.SetItemWhitelistEntry( name, true );
-			}
-
-			this.CreateLicensePacks();
-
-			if( this.Config.RemoveRewardsGrinding ) {
-				RewardsAPI.GetModSettings().GrindKillMultiplier = 0f;
-			}
-			if( this.Config.ForceSpawnWayfarer ) {
-				RewardsAPI.SpawnWayfarer( false );
-			}
-
-			// Finish loading Nihilism
-			TmlLoadHelpers.AddCustomPromise( "LicensesOnEnterWorld", () => {
-				NihilismAPI.NihilateCurrentWorld();
-
-				InboxMessages.ReadMessage( "nihilism_init" );
-
-				return false;
-			} );
-
-			TmlLoadHelpers.TriggerCustomPromise( "LicensesOnGameModeLoad" );
-			TmlLoadHelpers.AddWorldUnloadOncePromise( () => {
-				TmlLoadHelpers.ClearCustomPromise( "LicensesOnGameModeLoad" );
-			} );
-		}
-
-
-		private void CreateLicensePacks() {
-			var lic_def = new ShopPackItemDefinition {
-				Name = "License",
-				Stack = this.Config.LicensesPerPack,
-				CrimsonWorldOnly = null
-			};
-			var wild_lic_def = new ShopPackItemDefinition {
-				Name = "Wildcard License",
-				Stack = this.Config.WildcardLicensesPerPack,
-				CrimsonWorldOnly = null
-			};
-
-			var def1 = new ShopPackDefinition {
-				Name = "Standard License Pack",
-				Price = this.Config.LicensePackCostInPP,
-				Items = new ShopPackItemDefinition[] { lic_def }
-			};
-			var def2 = new ShopPackDefinition {
-				Name = "Wildcard License Pack",
-				Price = this.Config.WildcardLicensePackCostInPP,
-				Items = new ShopPackItemDefinition[] { wild_lic_def }
-			};
-			
-			if( this.Config.ResetWayfarerShop ) {
-				RewardsAPI.ShopClear();
-			}
-
-			if( this.Config.LicensesPerPack > 0 ) {
-				RewardsAPI.ShopAddPack( def1 );
-			}
-			if( this.Config.WildcardLicensesPerPack > 0 ) {
-				RewardsAPI.ShopAddPack( def2);
-			}
 		}
 	}
 }
---
+++ LicensesMod_Load.cs
@@ -0,0 +1,97 @@
+﻿using HamstarHelpers.DebugHelpers;
+using HamstarHelpers.Services.Messages;
+using HamstarHelpers.TmlHelpers;
+using Nihilism;
+using Rewards;
+using Rewards.Items;
+using Terraria.ModLoader;
+
+
+namespace Licenses {
+    public partial class LicensesMod : Mod {
+		public void LoadGameMode() {
+			NihilismAPI.SuppressAutoSavingOn();
+			RewardsAPI.SuppressConfigAutoSavingOn();
+			
+			this.LoadNihilismFilters();
+			this.LoadLicensePacks();
+
+			if( this.Config.RemoveRewardsGrinding ) {
+				RewardsAPI.GetModSettings().GrindKillMultiplier = 0f;
+			}
+			if( this.Config.ForceSpawnWayfarer ) {
+				RewardsAPI.SpawnWayfarer( false );
+			}
+
+			// Finish loading Nihilism
+			TmlLoadHelpers.AddCustomPromise( "LicensesOnEnterWorld", () => {
+				NihilismAPI.NihilateCurrentWorld();
+
+				InboxMessages.ReadMessage( "nihilism_init" );
+
+				return false;
+			} );
+
+			TmlLoadHelpers.TriggerCustomPromise( "LicensesOnGameModeLoad" );
+			TmlLoadHelpers.AddWorldUnloadOncePromise( () => {
+				TmlLoadHelpers.ClearCustomPromise( "LicensesOnGameModeLoad" );
+			} );
+		}
+
+
+		private void LoadNihilismFilters() {
+			if( this.Config.OverrideNihilismDefaultFilters ) {
+				if( this.Config.FreeRecipes ) {
+					NihilismAPI.ClearRecipeBlacklist( true );
+				} else {
+					NihilismAPI.SetRecipeBlacklistEntry( "Any Item", true );
+				}
+				NihilismAPI.ClearItemBlacklist( true );
+				NihilismAPI.ClearNpcBlacklist( true );
+				NihilismAPI.ClearNpcLootBlacklist( true );
+			}
+
+			NihilismAPI.SetItemBlacklistEntry( "All Items", true );
+
+			foreach( string name in this.Config.FreeStarterItems ) {
+				NihilismAPI.SetItemWhitelistEntry( name, true );
+			}
+		}
+
+
+		private void LoadLicensePacks() {
+			var lic_def = new ShopPackItemDefinition {
+				Name = "License",
+				Stack = this.Config.LicensesPerPack,
+				CrimsonWorldOnly = null
+			};
+			var wild_lic_def = new ShopPackItemDefinition {
+				Name = "Wildcard License",
+				Stack = this.Config.WildcardLicensesPerPack,
+				CrimsonWorldOnly = null
+			};
+
+			var def1 = new ShopPackDefinition {
+				Name = "Standard License Pack",
+				Price = this.Config.LicensePackCostInPP,
+				Items = new ShopPackItemDefinition[] { lic_def }
+			};
+			var def2 = new ShopPackDefinition {
+				Name = "Wildcard License Pack",
+				Price = this.Config.WildcardLicensePackCostInPP,
+				Items = new ShopPackItemDefinition[] { wild_lic_def }
+			};
+			
+			if( this.Config.ResetWayfarerShop ) {
+				RewardsAPI.ShopClear();
+			}
+
+			if( this.Config.LicensesPerPack > 0 ) {
+				RewardsAPI.ShopAddPack( def1 );
+			}
+			if( this.Config.WildcardLicensesPerPack > 0 ) {
+				RewardsAPI.ShopAddPack( def2);
+			}
+		}
+	}
+}
